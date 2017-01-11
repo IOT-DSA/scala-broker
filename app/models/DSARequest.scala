24c9c64ce5d621d46f9e450b22cc24415720bdfd
Vlad Orzhekhovskiy <vlad@uralian.com>
2017-01-11 11:44:48 -0500

    Implement a new version of DSARequest hierarchy and play-json mappings

---
+++ app/models/DSARequest.scala
@@ -0,0 +1,88 @@
+package models
+
+import DSAValue._
+
+/**
+ * Supported DSA methods.
+ */
+object DSAMethod extends Enumeration {
+  type DSAMethod = Value
+
+  val List = Value("list")
+  val Set = Value("set")
+  val Remove = Value("remove")
+  val Invoke = Value("invoke")
+  val Subscribe = Value("subscribe")
+  val Unsubscribe = Value("unsubscribe")
+  val Close = Value("close")
+}
+import DSAMethod._
+
+/**
+ * Base trait for DSA requests.
+ */
+sealed trait DSARequest extends Serializable {
+  val rid: Int
+  val method: DSAMethod
+}
+
+/**
+ * LIST request.
+ */
+case class ListRequest(rid: Int, path: String) extends DSARequest {
+  val method = List
+}
+
+/**
+ * SET request.
+ */
+case class SetRequest(rid: Int, path: String, value: DSAVal, permit: Option[String] = None) extends DSARequest {
+  val method = Set
+}
+
+/**
+ * REMOVE request.
+ */
+case class RemoveRequest(rid: Int, path: String) extends DSARequest {
+  val method = Remove
+}
+
+/**
+ * INVOKE request.
+ */
+case class InvokeRequest(rid: Int, path: String, params: DSAMap = Map.empty,
+                         permit: Option[String] = None) extends DSARequest {
+  val method = Invoke
+}
+
+/**
+ * SUBSCRIBE request.
+ */
+case class SubscriptionPath(path: String, sid: Int, qos: Option[Int] = None)
+case class SubscribeRequest(rid: Int, paths: List[SubscriptionPath]) extends DSARequest {
+  val method = Subscribe
+  lazy val path = paths.head.ensuring(paths.size == 1)
+  def split = paths.map(SubscribeRequest(rid, _))
+}
+object SubscribeRequest {
+  def apply(rid: Int, paths: SubscriptionPath*): SubscribeRequest = apply(rid, paths.toList)
+}
+
+/**
+ * UNSUBSCRIBE request.
+ */
+case class UnsubscribeRequest(rid: Int, sids: List[Int]) extends DSARequest {
+  val method = Unsubscribe
+  lazy val sid = sids.head.ensuring(sids.size == 1)
+  def split = sids.map(UnsubscribeRequest(rid, _))
+}
+object UnsubscribeRequest {
+  def apply(rid: Int, sids: Int*): UnsubscribeRequest = apply(rid, sids.toList)
+}
+
+/**
+ * CLOSE request.
+ */
+case class CloseRequest(rid: Int) extends DSARequest {
+  val method = Close
+}
--- app/models/package.scala
+++ app/models/package.scala
@@ -1,18 +1,45 @@
 import java.util.Base64
 
-import models.DSAValue
 import play.api.data.validation.ValidationError
+import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
+/**
+ * Types and utility functions for models.
+ */
 package object models {
   import DSAValue._
 
   val BinaryPrefix = "\u001Bbytes:"
 
   /**
-   * DSAValue -> JSON
+   * Implements JSON Reads for the given Enumeration type.
    */
-  implicit val DSAValueWrites: Writes[DSAVal] = new Writes[DSAVal] {
+  def enumReads[E <: Enumeration](enum: E): Reads[E#Value] = new Reads[E#Value] {
+    def reads(json: JsValue): JsResult[E#Value] = json match {
+      case JsString(s) => {
+        try {
+          JsSuccess(enum.withName(s))
+        } catch {
+          case _: NoSuchElementException => JsError(s"Enumeration expected of type: '${enum.getClass}', but it does not appear to contain the value: '$s'")
+        }
+      }
+      case _ => JsError("String value expected")
+    }
+  }
+
+  /**
+   * Implements JSON Writes for the given Enumeration type.
+   */
+  implicit def enumWrites[E <: Enumeration]: Writes[E#Value] = new Writes[E#Value] {
+    def writes(v: E#Value): JsValue = JsString(v.toString)
+  }
+
+  /**
+   * DSAValue <-> JSON
+   */
+  implicit val DSAValueFormat: Format[DSAVal] = new Format[DSAVal] {
+
     def writes(dsa: DSAVal) = dsa match {
       case v: NumericValue => Json.toJson(v.value)
       case v: StringValue  => Json.toJson(v.value)
@@ -21,16 +48,9 @@ package object models {
       case v: MapValue     => Json.toJson(valueMapToJson(v.value))
       case v: ArrayValue   => Json.toJson(valueListToJson(v.value))
     }
-  }
-
-  /**
-   * JSON -> DSAValue
-   */
-  implicit val DSAValueReads: Reads[DSAVal] = new Reads[DSAVal] {
 
     def reads(json: JsValue) = try {
-      val dsa = json2dsa(json)
-      JsSuccess(dsa)
+      JsSuccess(json2dsa(json))
     } catch {
       case e: RuntimeException => JsError(ValidationError(e.getMessage))
     }
@@ -43,6 +63,60 @@ package object models {
       case JsObject(fields)                          => new MapValue(jsonMapToValues(fields.toMap))
       case JsArray(items)                            => new ArrayValue(jsonListToValues(items))
     }
+  }
+
+  /**
+   * DSARequest <-> JSON
+   */
+  implicit val DSARequestFormat: Format[DSARequest] = new Format[DSARequest] {
+    import DSAMethod._
+
+    implicit val SubscriptionPathFormat = Json.format[SubscriptionPath]
+
+    implicit val DSAMethodReads = enumReads(DSAMethod)
+
+    val ListRequestFormat = Json.format[ListRequest]
+    val SetRequestFormat = (
+      (__ \ 'rid).format[Int] ~
+      (__ \ 'path).format[String] ~
+      (__ \ 'value).format[DSAVal] ~
+      (__ \ 'permit).formatNullable[String])(SetRequest.apply, unlift(SetRequest.unapply))
+    val RemoveRequestFormat = Json.format[RemoveRequest]
+    val InvokeRequestFormat = (
+      (__ \ 'rid).format[Int] ~
+      (__ \ 'path).format[String] ~
+      (__ \ 'params).formatNullable[DSAMap].inmap(emptyIfNone, noneIfEmpty) ~
+      (__ \ 'permit).formatNullable[String])(InvokeRequest.apply, unlift(InvokeRequest.unapply))
+    val SubscribeRequestFormat = Json.format[SubscribeRequest]
+    val UnsubscribeRequestFormat = Json.format[UnsubscribeRequest]
+    val CloseRequestFormat = Json.format[CloseRequest]
+
+    def writes(req: DSARequest) = baseJson(req) ++ (req match {
+      case x: ListRequest        => ListRequestFormat.writes(x)
+      case x: SetRequest         => SetRequestFormat.writes(x)
+      case x: RemoveRequest      => RemoveRequestFormat.writes(x)
+      case x: InvokeRequest      => InvokeRequestFormat.writes(x)
+      case x: SubscribeRequest   => SubscribeRequestFormat.writes(x)
+      case x: UnsubscribeRequest => UnsubscribeRequestFormat.writes(x)
+      case x: CloseRequest       => CloseRequestFormat.writes(x)
+    }).as[JsObject]
+
+    def reads(json: JsValue) = (json \ "method").as[DSAMethod] match {
+      case List        => ListRequestFormat.reads(json)
+      case Set         => SetRequestFormat.reads(json)
+      case Remove      => RemoveRequestFormat.reads(json)
+      case Invoke      => InvokeRequestFormat.reads(json)
+      case Subscribe   => SubscribeRequestFormat.reads(json)
+      case Unsubscribe => UnsubscribeRequestFormat.reads(json)
+      case Close       => CloseRequestFormat.reads(json)
+      case unknown     => JsError(s"Unknown method: '$unknown'")
+    }
+
+    private def baseJson(req: DSARequest) = Json.obj("rid" -> req.rid, "method" -> req.method)
+
+    private def noneIfEmpty(map: DSAMap) = if (map.isEmpty) None else Some(map)
+
+    private def emptyIfNone(opt: Option[DSAMap]) = opt getOrElse Map.empty
   }
 
   /* helpers */
