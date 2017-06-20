d29a70366ea949f7c71306d9d67b53d29900c5e9
Vlad Orzhekhovskiy <vlad@uralian.com>
2017-06-20 12:51:52 -0400

    Implement downstream node DSA functionality

--- app/models/akka/DownstreamActor.scala
+++ app/models/akka/DownstreamActor.scala
@@ -6,6 +6,10 @@ import scala.util.control.NonFatal
 
 import akka.actor.{ Actor, ActorLogging, Props, Status, actorRef2Scala }
 import models.Settings
+import models.rpc.DSAValue.{ BooleanValue, StringValue, array, obj }
+import models.RequestEnvelope
+import models.rpc._
+import models.ResponseEnvelope
 
 /**
  * Actor for DSA `/downstream` node.
@@ -17,13 +21,16 @@ import models.Settings
  */
 class DownstreamActor(settings: Settings) extends Actor with ActorLogging {
   import DownstreamActor._
+  import StreamState._
 
   assert(self.path.name == settings.Nodes.Downstream,
     s"Downstream actor should be created under name ${settings.Nodes.Downstream}")
 
-  override def preStart = log.debug("[/downstream] actor created")
+  private val ownId = settings.Paths.Downstream
 
-  override def postStop = log.debug("[/downstream] actor stopped")
+  override def preStart = log.debug(s"[$ownId] actor created")
+
+  override def postStop = log.debug(s"[$ownId] actor stopped")
 
   def receive = {
     case GetDSLink(name) => sender ! context.child(name)
@@ -49,6 +56,10 @@ class DownstreamActor(settings: Settings) extends Actor with ActorLogging {
       val filtered = context.children.filter(ref => pattern.matcher(ref.path.name).matches)
       val result = filtered.drop(offset).take(limit).map(_.path.name)
       sender ! result
+
+    case RequestEnvelope(from, _, requests) =>
+      val envelopes = requests flatMap (processRequest(from, _))
+      envelopes foreach (sender ! _)
   }
 
   /**
@@ -63,6 +74,35 @@ class DownstreamActor(settings: Settings) extends Actor with ActorLogging {
     }
     log.debug(s"DSLink[${child.path.name}] created for ${ci.linkName}")
     child
+  }
+
+  /**
+   * Processes an incoming request and produces a list of response envelopes, if any.
+   */
+  private def processRequest(from: String, request: DSARequest): TraversableOnce[ResponseEnvelope] = request match {
+    case ListRequest(rid, _) =>
+      listNodes grouped settings.ChildrenPerListResponse map { rows =>
+        val response = DSAResponse(rid = rid, stream = Some(Open), updates = Some(rows.toList))
+        ResponseEnvelope(ownId, from, List(response))
+      }
+    case CloseRequest(rid) =>
+      List(ResponseEnvelope(ownId, from, List(DSAResponse(rid = rid, stream = Some(Closed)))))
+    case _ =>
+      log.error(s"$ownId: invalid request - $request")
+      Nil
+  }
+
+  /**
+   * Generates response for LIST request.
+   */
+  private def listNodes = {
+    val configs = rows(IsNode, "downstream" -> true).toIterable
+
+    val children = context.children map { child =>
+      array(child.path.name, obj(IsNode))
+    }
+
+    configs ++ children
   }
 }
 
