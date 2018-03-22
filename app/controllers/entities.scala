e1c891b743ebaca65728e5a87c84e580ea157f93
Vlad Orzhekhovskiy <vlad@uralian.com>
2018-03-22 00:03:32 -0400

    Implement BenchmarkConfig and custom binder for it

--- app/controllers/entities.scala
+++ app/controllers/entities.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import akka.actor.Address
+import models.bench.BenchmarkActor.RequesterTarget
 
 /**
  * Object passed to a view to supply various page information.
@@ -12,3 +13,24 @@ case class ViewConfig(title: String, navItem: String, heading: String, descripti
  * Broker node info.
  */
 case class NodeInfo(address: Address, leader: Boolean, uid: Long, roles: Set[String], status: String)
+
+/**
+ * Benchmark configuration.
+ */
+case class BenchmarkConfig(subscribe: Boolean, reqCount: Int, rspCount: Int, rspNodeCount: Int,
+                           batchSize: Int, batchTimeout: Long, parseJson: Boolean) {
+
+  val expectedInvokeRate = (reqCount * batchSize * 1000L / batchTimeout).toInt
+
+  def calculateExpectedUpdateToInvokeRatio(targets: Iterable[RequesterTarget]) = if (subscribe) {
+    val expectedEvents = targets.groupBy(identity).map(_._2.size).map(a => a * a).sum
+    (expectedEvents + reqCount).toDouble / reqCount
+  } else 1.0
+}
+
+/**
+ * Factory for [[BenchmarkConfig]] instances.
+ */
+object BenchmarkConfig {
+  val Default = BenchmarkConfig(false, 5, 5, 10, 100, 1000L, false)
+}
