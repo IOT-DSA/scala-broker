c0d6cdeca502ab29111a7bc57341529d9832810d
Vitaliy Ananyev <vananyev@ninthdecimal.com>
2018-05-03 22:42:24 +0300

    145084: Add draft of transformer class for msgpack, add test case for it.

---
+++ test/models/rpc/DSAValueMsgpackSpec.scala
@@ -0,0 +1,36 @@
+package models.rpc
+
+import akka.NotUsed
+import org.scalatestplus.play.PlaySpec
+
+class DSAValueMsgpackSpec extends PlaySpec {
+//  import models.rpc.MessageFlowTransformer.jsonMessageFlowTransformer
+
+  "Msgpack transformer" should {
+    "convert play js value to/from http payload" in {
+
+      import akka.stream.scaladsl._
+      import play.api.libs.json._
+      import play.api.http.websocket._
+
+//      val jsonTransformer = models.rpc.MessageFlowTransformer.jsonMessageFlowTransformer
+      val msgTransformer = models.rpc.MessageFlowTransformer2.msgpackMessageFlowTransformer
+
+      val source1 = Source(1 to 3)
+      val list2 = List(JsNumber(1), JsNumber(2))
+      val source2 = Source(list2)
+      val flow2: Flow[JsValue, JsValue, _] = Flow[JsValue].map(x => x)
+      val source3 = source2 via flow2
+      val resultFlow = msgTransformer.transform(flow2)
+
+      val source4 = Source(List(TextMessage("Message1")))
+      val resultSrc = source4 via resultFlow
+
+      val sink4 = Sink.foreach[TextMessage](println(_))
+
+//      val runnable1 = resultSrc to sink4
+
+      println("That's all")
+    }
+  }
+}
