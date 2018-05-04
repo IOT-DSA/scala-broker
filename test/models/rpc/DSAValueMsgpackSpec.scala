ae859df3ae2d98a73ab7463adfb528b4fa46ca71
Vitaliy Ananyev <vananyev@ninthdecimal.com>
2018-05-04 19:13:43 +0300

    145084: Add test line in unit test

--- test/models/rpc/DSAValueMsgpackSpec.scala
+++ test/models/rpc/DSAValueMsgpackSpec.scala
@@ -19,16 +19,16 @@ class DSAValueMsgpackSpec extends PlaySpec {
       val source1 = Source(1 to 3)
       val list2 = List(JsNumber(1), JsNumber(2))
       val source2 = Source(list2)
-      val flow2: Flow[JsValue, JsValue, _] = Flow[JsValue].map(x => x)
+      val flow2: Flow[JsValue, JsValue, _] = Flow[JsValue].map(x => { println("AAAA"); x })
       val source3 = source2 via flow2
       val resultFlow = msgTransformer.transform(flow2)
 
-      val source4 = Source(List(TextMessage("Message1")))
+      val source4 = Source(List(TextMessage("Json message 1")))
       val resultSrc = source4 via resultFlow
 
       val sink4 = Sink.foreach[TextMessage](println(_))
 
-//      val runnable1 = resultSrc to sink4
+      val runnable1 = Flow.fromSinkAndSource(sink4, resultSrc)
 
       println("That's all")
     }
