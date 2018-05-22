3b64296687ea213aa4278a0b0bdd696adb548349
dmitryisaev <dmitryisaev@cloud.upwork.com>
2018-05-22 16:52:54 +0300

    nothing very important there

---
+++ test/models/akka/distributed/data/DataStateKeeperSpec.scala
@@ -0,0 +1,11 @@
+package models.akka.distributed.data
+
+import org.scalatest.{GivenWhenThen, Matchers, WordSpecLike}
+
+class DataStateKeeperSpec extends WordSpecLike
+  with Matchers
+  with GivenWhenThen {
+
+
+
+}
