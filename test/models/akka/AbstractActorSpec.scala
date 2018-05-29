c18fc45e455cd21054e48a15129cc957a283ab8c
dmitryisaev <dmitryisaev@cloud.upwork.com>
2018-05-29 19:57:14 +0300

    test failures fix

--- test/models/akka/AbstractActorSpec.scala
+++ test/models/akka/AbstractActorSpec.scala
@@ -1,18 +1,25 @@
 package models.akka
 
-import org.scalatest.{ BeforeAndAfterAll, MustMatchers, OptionValues, WordSpecLike }
+import org.scalatest.{BeforeAndAfterAll, MustMatchers, OptionValues, WordSpecLike}
 import org.scalatest.concurrent.ScalaFutures
-
 import akka.actor.ActorSystem
-import akka.testkit.{ ImplicitSender, TestKit }
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.time.{Millis, Seconds, Span}
 
 /**
- * Base class for testing actors.
- */
+  * Base class for testing actors.
+  */
 abstract class AbstractActorSpec extends TestKit(ActorSystem()) with ImplicitSender
   with WordSpecLike with MustMatchers with BeforeAndAfterAll with ScalaFutures
   with OptionValues {
 
   override def afterAll = TestKit.shutdownActorSystem(system)
+
+  /**
+    * The simple tune to avoid some issues for asynchronous operations in unit tests.
+    * Probably the amount of timeout and interval should be figured out experimentally.
+    */
+  override implicit val patienceConfig = PatienceConfig(timeout = Span(1, Seconds), interval = Span(500, Millis))
+
 
 }
