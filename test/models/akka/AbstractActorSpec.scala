f809be201a8e1e3ec34dd5d8c1a2cc47147ebb86
ilya-martynov <ilya.mar2nov@gmail.com>
2018-06-04 12:42:02 +0300

    Uncomment the failed tests [tune the time inteval #2]

--- test/models/akka/AbstractActorSpec.scala
+++ test/models/akka/AbstractActorSpec.scala
@@ -19,7 +19,7 @@ abstract class AbstractActorSpec extends TestKit(ActorSystem()) with ImplicitSen
     * The simple tune to avoid some issues for asynchronous operations in unit tests.
     * Probably the amount of timeout and interval should be figured out experimentally.
     */
-  override implicit val patienceConfig = PatienceConfig(timeout = Span(150, Millis), interval = Span(30, Millis))
+  override implicit val patienceConfig = PatienceConfig(timeout = Span(300, Millis), interval = Span(100, Millis))
 
   override def afterAll = TestKit.shutdownActorSystem(system)
 
