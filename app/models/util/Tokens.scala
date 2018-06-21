391022cf955815cbc2847ac712519adea048fdd6
Vitaliy Ananyev <vananyev@ninthdecimal.com>
2018-06-21 20:14:18 +0300

    152467: Add token utils

---
+++ app/models/util/Tokens.scala
@@ -0,0 +1,38 @@
+package models.util
+
+import models.api.DSANode
+
+import scala.concurrent.Future
+import scala.util.Random
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Tokens {
+  def makeToken(dsaNode: DSANode) : Future[String] = {
+    val token = createToken()
+    val tokenId = token.substring(0, 16);
+
+    dsaNode.child(tokenId).flatMap {
+        case None => Future(token)
+        case _ => makeToken(dsaNode)
+    }
+  }
+
+  def createToken(): String = {
+    val tokenCodes = Array[Byte](48)
+    var i = 0;
+    while (i < 48) {
+      val n = Random.nextInt(Byte.MaxValue)
+      if ((n >= 0x30 && n <= 0x39) ||
+        (n >= 0x41 && n <= 0x5A) ||
+        (n >= 0x61 && n <= 0x7A)) {
+        tokenCodes(i) = n.toByte;
+        i+=1
+      }
+    }
+    val rslt = new String(tokenCodes, "UTF-8")
+
+    rslt
+  }
+
+}
